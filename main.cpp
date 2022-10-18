9150624cb4884eceb36e31ebc3343df0f2697cd4
SandiYunus <114415397+SandiYunus@users.noreply.github.com>
2022-10-19 06:48:04 +0800

    Initialisasi by sandi

--- main.cpp
+++ main.cpp
@@ -118,7 +118,7 @@ void update(int value)
     glutPostRedisplay();
     glutTimerFunc(25, update, 0); // kalau mau kasih cepat berputar kasih turun angkanya yg 25 kalau mau perlamabat ya sebaliknya
 }
-// Laode Yunus Abdillah Sandi
+// Laode Yunus Abdillah Sandii
 int main(int argc, char **argv)
 {
     // Initialize GLUT
